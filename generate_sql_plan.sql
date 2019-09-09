9da78aade4508ffaf717a0c2b9ca293af1e63b46
heccrbq <42538722+heccrbq@users.noreply.github.com>
2019-09-09 16:59:35 +0300

    Create generate_sql_plan.sql

---
+++ generate_sql_plan.sql
@@ -0,0 +1,20 @@
+select 
+    plan_hash_value,
+    id,
+    lpad(' ', depth) || operation operation,
+    options,
+    object_owner,
+    object_name,
+    p.object_type,
+    optimizer,
+    cost,
+    p.cardinality,
+    p.bytes,
+    p.cpu_cost,
+    p.io_cost,
+    access_predicates,
+    filter_predicates,
+    p.parent_id
+from dba_hist_sql_plan p
+where sql_id = '3srshtyjrcghw'
+order by plan_hash_value, id
