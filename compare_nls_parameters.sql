815ae385051c9b2520c19f57236616b805bfbd57
heccrbq <42538722+heccrbq@users.noreply.github.com>
2019-09-10 17:00:42 +0300

    Update compare_nls_parameters.sql

--- compare_nls_parameters.sql
+++ compare_nls_parameters.sql
@@ -3,9 +3,9 @@ select
     ndp.value database_value,
     nip.value instance_value,
     case when nip.value = ndp.value or nip.value is null then 'SUCCESS' else 'FAIL' end instance_value_comparison,
-    np.value client_value,
-    case when np.value = ndp.value or np.value is null then 'SUCCESS' else 'FAIL' end client_value_comparison
+    nsp.value session_value,
+    case when nsp.value = ndp.value or nsp.value is null then 'SUCCESS' else 'FAIL' end session_value_comparison
 from nls_database_parameters ndp 
     left join nls_instance_parameters nip on ndp.parameter = nip.parameter
-    left join v$nls_parameters np on ndp.parameter = np.parameter
-order by parameter;
+    left join nls_session_parameters nsp on ndp.parameter = nsp.parameter
+order by ndp.parameter;
