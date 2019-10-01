9e5241738535b101ea1c86df24abf60cd09332e5
heccrbq <42538722+heccrbq@users.noreply.github.com>
2019-10-01 09:51:58 +0300

    Update table_common_info.sql

--- one-time scripts/table_common_info.sql
+++ one-time scripts/table_common_info.sql
@@ -10,6 +10,7 @@
  *   - наименование таблицы
  *   - Количество строк по статистике
  *   - Занимаемое место сегментом
+ *   - Ялвяется ли таблица GTT
  *   - Партицирована ли таблица
  *   - Тип партицирования
  *   - Поля партицирования
@@ -31,7 +32,7 @@ select dt.owner,
 from dba_tables dt
   left join dba_part_tables dpt on dpt.owner = dt.owner and dpt.table_name = dt.table_name
   left join dba_indexes di on di.table_owner = dt.owner and di.table_name = dt.table_name
-  inner join dba_segments ds on (ds.owner, ds.segment_name) in ((dt.owner, dt.table_name), (di.owner, di.index_name))
+  left join dba_segments ds on ds.owner = dt.owner and ds.segment_name = dt.table_name
 where dt.owner = 'OWNER_DWH' /*:p_table_owner*/
   and dt.table_name = 'FT_CREDIT_BUREAU_DATA_TT' /*:p_table_table*/
 group by dt.owner,
