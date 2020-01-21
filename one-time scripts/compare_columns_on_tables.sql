5ce24029e1aa47daa6ddea582838a862597dd5e2
heccrbq <42538722+heccrbq@users.noreply.github.com>
2020-01-21 16:01:23 +0300

    Update compare_columns_on_tables.sql

--- one-time scripts/compare_columns_on_tables.sql
+++ one-time scripts/compare_columns_on_tables.sql
@@ -30,8 +30,8 @@
 with settings as 
 (
     select
-        'HCF_TSETTINGS' AS comparable_table_1,
-        'TENTRY' AS comparable_table_2,
+        'DROPME' AS comparable_table_1,
+        'DROPME2' AS comparable_table_2,
         'ALL' AS compare_columns,    -- case insensitive [ALL | INCLUDE <column_name_1>, <column_name_2>, ... <column_name_n> | EXCLUDE <column_name_1>, <column_name_2>, ... <column_name_n>]
         'Y' AS compare_ddl_columns,
 --        'Y' AS compare_cmnt_columns,
@@ -69,23 +69,23 @@ comp_ddl_col as
             when utc1.table_name is null or utc2.table_name is null then
                 'В таблице ' || nvl2(utc1.table_name, s.comparable_table_2, s.comparable_table_1) || ' нет поля ' || nvl(utc1.column_name, utc2.column_name)
             else 
-                decode(utc1.nullable,              utc2.nullable,              null,  'nullable('              || utc1.nullable              || ' vs ' || utc2.nullable              || ')' || crlf) ||
-                decode(utc1.char_used,             utc2.char_used,             null,  'char_used('             || utc1.char_used             || ' vs ' || utc2.char_used             || ')' || crlf) ||
-                decode(utc1.collation,             utc2.collation,             null,  'collation('             || utc1.collation             || ' vs ' || utc2.collation             || ')' || crlf) ||
-                decode(utc1.column_id,             utc2.column_id,             null,  'column_id('             || utc1.column_id             || ' vs ' || utc2.column_id             || ')' || crlf) ||
-                decode(utc1.data_type,             utc2.data_type,             null,  'data_type('             || utc1.data_type             || ' vs ' || utc2.data_type             || ')' || crlf) ||
-                decode(utc1.data_scale,            utc2.data_scale,            null,  'data_scale('            || utc1.data_scale            || ' vs ' || utc2.data_scale            || ')' || crlf) ||
-                decode(utc1.char_length,           utc2.char_length,           null,  'char_length('           || utc1.char_length           || ' vs ' || utc2.char_length           || ')' || crlf) ||
-                decode(utc1.data_length,           utc2.data_length,           null,  'data_length('           || utc1.data_length           || ' vs ' || utc2.data_length           || ')' || crlf) ||
-                decode(utc3.data_default,          utc4.data_default,          null,  'data_default('          || utc3.data_default          || ' vs ' || utc4.data_default          || ')' || crlf) ||
-                decode(utc1.data_type_mod,         utc2.data_type_mod,         null,  'data_type_mod('         || utc1.data_type_mod         || ' vs ' || utc2.data_type_mod         || ')' || crlf) ||
-                decode(utc1.data_precision,        utc2.data_precision,        null,  'data_precision('        || utc1.data_precision        || ' vs ' || utc2.data_precision        || ')' || crlf) ||
-                decode(utc1.default_length,        utc2.default_length,        null,  'default_length('        || utc1.default_length        || ' vs ' || utc2.default_length        || ')' || crlf) ||
-                decode(utc1.default_on_null,       utc2.default_on_null,       null,  'default_on_null('       || utc1.default_on_null       || ' vs ' || utc2.default_on_null       || ')' || crlf) ||
-                decode(utc1.identity_column,       utc2.identity_column,       null,  'identity_column('       || utc1.identity_column       || ' vs ' || utc2.identity_column       || ')' || crlf) ||
-                decode(utc1.data_type_owner,       utc2.data_type_owner,       null,  'data_type_owner('       || utc1.data_type_owner       || ' vs ' || utc2.data_type_owner       || ')' || crlf) ||
-                decode(utc1.character_set_name,    utc2.character_set_name,    null,  'character_set_name('    || utc1.character_set_name    || ' vs ' || utc2.character_set_name    || ')' || crlf) ||
-                decode(utc1.char_col_decl_length,  utc2.char_col_decl_length,  null,  'char_col_decl_length('  || utc1.char_col_decl_length  || ' vs ' || utc2.char_col_decl_length  || ')')
+                decode(utc1.nullable,              utc2.nullable,              null,  'nullable('              ||  utc1.nullable              || ' vs ' ||  utc2.nullable              || ')' || crlf) ||
+                decode(utc1.char_used,             utc2.char_used,             null,  'char_used('             ||  utc1.char_used             || ' vs ' ||  utc2.char_used             || ')' || crlf) ||
+                decode(utc1.collation,             utc2.collation,             null,  'collation('             ||  utc1.collation             || ' vs ' ||  utc2.collation             || ')' || crlf) ||
+                decode(utc1.column_id,             utc2.column_id,             null,  'column_id('             ||  utc1.column_id             || ' vs ' ||  utc2.column_id             || ')' || crlf) ||
+                decode(utc1.data_type,             utc2.data_type,             null,  'data_type('             ||  utc1.data_type             || ' vs ' ||  utc2.data_type             || ')' || crlf) ||
+                decode(utc1.data_scale,            utc2.data_scale,            null,  'data_scale('            ||  utc1.data_scale            || ' vs ' ||  utc2.data_scale            || ')' || crlf) ||
+                decode(utc1.char_length,           utc2.char_length,           null,  'char_length('           ||  utc1.char_length           || ' vs ' ||  utc2.char_length           || ')' || crlf) ||
+                decode(utc1.data_length,           utc2.data_length,           null,  'data_length('           ||  utc1.data_length           || ' vs ' ||  utc2.data_length           || ')' || crlf) ||
+                decode(utc3.data_default,          utc4.data_default,          null,  'data_default('          ||  utc3.data_default          || ' vs ' ||  utc4.data_default          || ')' || crlf) ||
+                decode(utc1.data_type_mod,         utc2.data_type_mod,         null,  'data_type_mod('         ||  utc1.data_type_mod         || ' vs ' ||  utc2.data_type_mod         || ')' || crlf) ||
+                decode(utc1.data_precision,        utc2.data_precision,        null,  'data_precision('        ||  utc1.data_precision        || ' vs ' ||  utc2.data_precision        || ')' || crlf) ||
+                decode(utc1.default_length,        utc2.default_length,        null,  'default_length('        ||  utc1.default_length        || ' vs ' ||  utc2.default_length        || ')' || crlf) ||
+                decode(utc1.default_on_null,       utc2.default_on_null,       null,  'default_on_null('       ||  utc1.default_on_null       || ' vs ' ||  utc2.default_on_null       || ')' || crlf) ||
+                decode(utc1.identity_column,       utc2.identity_column,       null,  'identity_column('       ||  utc1.identity_column       || ' vs ' ||  utc2.identity_column       || ')' || crlf) ||
+                decode(utc1.data_type_owner,       utc2.data_type_owner,       null,  'data_type_owner('       ||  utc1.data_type_owner       || ' vs ' ||  utc2.data_type_owner       || ')' || crlf) ||
+                decode(utc1.character_set_name,    utc2.character_set_name,    null,  'character_set_name('    ||  utc1.character_set_name    || ' vs ' ||  utc2.character_set_name    || ')' || crlf) ||
+                decode(utc1.char_col_decl_length,  utc2.char_col_decl_length,  null,  'char_col_decl_length('  ||  utc1.char_col_decl_length  || ' vs ' ||  utc2.char_col_decl_length  || ')')
         end reason
     -- user_tab_columns не содержит UNUSED полей, но они нам не особо то и нужны
     from settings s
