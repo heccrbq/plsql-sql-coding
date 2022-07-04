8f64198643e7414e4f94653767032ca8e86a8cfc
heccrbq <42538722+heccrbq@users.noreply.github.com>
2022-07-04 11:58:34 +0300

    Create sql_monitoring_report.sql

---
+++ sqldev/sql_monitoring_report.sql
@@ -0,0 +1,20 @@
+/**
+ * =============================================================================================
+ * Запрос генерации Real-Time отчета по указанному sql_id
+ * =============================================================================================
+ * @param   sql_id   SQL_ID запроса
+ * =============================================================================================
+ * Описание полей:
+ *  - file#  : имя сгенерированного отчета
+ *  - output : содержание отчета
+ */
+with source as (
+    select '6hjs0624rwqyy' sql_id from dual
+--    select snap_id, begin_interval_time,end_interval_time from dba_hist_snapshot order by snap_id desc
+)
+select 
+    'SQLMONITOR_' || sql_id || '.html' file#,
+    dbms_sqltune.report_sql_monitor(sql_id =>       'sql_id', 
+                                    report_level => 'all', 
+                                    type =>         'html') output
+from source;
