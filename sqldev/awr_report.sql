e7d1d2bd270e4772aea40c2f986bdf94494f4108
heccrbq <42538722+heccrbq@users.noreply.github.com>
2022-07-06 15:00:27 +0300

    Update awr_report.sql

--- sqldev/awr_report.sql
+++ sqldev/awr_report.sql
@@ -10,18 +10,19 @@
  *  - output : содержание отчета
  */
 with source as (
-    select name, dbid, inst_id, 98566 start_snap_id, 98567 end_snap_id from gv$database
+    select 98566 start_snap_id, 98567 end_snap_id from dual
 --    select snap_id, begin_interval_time,end_interval_time from dba_hist_snapshot order by snap_id desc
 )
 select
-    'AWR_' || lower(s.name) || '_' || s.start_snap_id || '_' || s.end_snap_id || '.html' file#,
+    'AWR_' || lower(d.name) || '_' || d.inst_id || '_' || s.start_snap_id || '_' || s.end_snap_id || '.html' file#,
     dbms_xmlgen.convert(xmlagg(xmlelement(output, t.output || chr(10)) order by rownum).extract('//text()').getclobval(),1) output
 from source s,
-    table(dbms_workload_repository.awr_report_html(l_dbid     => s.dbid, 
-                                                   l_inst_num => s.inst_id, 
+    gv$database d,
+    table(dbms_workload_repository.awr_report_html(l_dbid     => d.dbid, 
+                                                   l_inst_num => d.inst_id, 
                                                    l_bid      => s.start_snap_id, 
                                                    l_eid      => s.end_snap_id)) t
-group by s.name, s.dbid, s.inst_id, s.start_snap_id, s.end_snap_id;
+group by d.name, d.dbid, d.inst_id, s.start_snap_id, s.end_snap_id;
 
 
 
