0c71a6725a171702665a1d674c73538d14656457
heccrbq <42538722+heccrbq@users.noreply.github.com>
2022-07-06 14:55:44 +0300

    Update awr_report.sql

--- sqldev/awr_report.sql
+++ sqldev/awr_report.sql
@@ -22,3 +22,41 @@ from source s,
                                                    l_bid      => s.start_snap_id, 
                                                    l_eid      => s.end_snap_id)) t
 group by s.name, s.dbid, s.inst_id, s.start_snap_id, s.end_snap_id;
+
+
+
+/**
+ * =============================================================================================
+ * Запрос генерации AWR отчета в формате HTML на текущей БД с указанием snapshot'ов
+ * =============================================================================================
+ * @param   start_snap_id (DATE)   Стартовое время генерации отчета
+ * @param   end_snap_id   (DATE)   Конечное время генерации отчета
+ * =============================================================================================
+ * Описание полей:
+ *  - file#  : имя сгенерированного отчета
+ *  - output : содержание отчета
+ */
+with source as (
+    select timestamp'2022-07-04 04:47:00' start_time, timestamp'2022-07-04 05:37:00' end_time from dual
+),
+awr as (
+    select 
+        d.name, d.dbid, d.inst_id, lt.start_snap_id, lt.end_snap_id
+    from gv$database d,
+    lateral(
+        select min(snap_id) start_snap_id, max(snap_id) end_snap_id
+        from dba_hist_snapshot sn, source s
+        where sn.dbid = d.dbid and sn.instance_number = d.inst_id 
+            and (s.start_time between sn.begin_interval_time and sn.end_interval_time 
+                or s.end_time between sn.begin_interval_time and sn.end_interval_time)) lt
+)
+--select * From awr;
+select
+    'AWR_' || lower(awr.name) || '_' || awr.inst_id || '_' || awr.start_snap_id || '_' || awr.end_snap_id || '.html' file#,
+    dbms_xmlgen.convert(xmlagg(xmlelement(output, t.output || chr(10)) order by rownum).extract('//text()').getclobval(),1) output
+from awr,
+    table(dbms_workload_repository.awr_report_html(l_dbid     => awr.dbid, 
+                                                   l_inst_num => awr.inst_id, 
+                                                   l_bid      => awr.start_snap_id, 
+                                                   l_eid      => awr.end_snap_id)) t
+group by awr.name, awr.dbid, awr.inst_id, awr.start_snap_id, awr.end_snap_id;
