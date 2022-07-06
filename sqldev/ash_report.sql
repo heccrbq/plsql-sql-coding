85734f12dcab7bd15ec7674e3eb8760f01a817bb
heccrbq <42538722+heccrbq@users.noreply.github.com>
2022-07-06 15:05:31 +0300

    Update ash_report.sql

--- sqldev/ash_report.sql
+++ sqldev/ash_report.sql
@@ -12,16 +12,17 @@
  *  - output : содержание отчета
  */
 with source as (
-    select name, dbid, inst_id, null start_date, null end_date, null sid, '9xf8bc1w04q4g' sql_id from gv$database
+    select null start_date, null end_date, null sid, '9xf8bc1w04q4g' sql_id from gv$database
 )
 select
-    'ASH_' || lower(s.name) || '_' || s.start_date || '_' || s.end_date || '.html' file#,
+    'ASH_' || lower(d.name) || '_' || d.inst_id || '_' || s.start_date || '_' || s.end_date || '_' || s.sid || '_' || s.sql_id || '.html' file#,
     dbms_xmlgen.convert(xmlagg(xmlelement(output, t.output || chr(10)) order by rownum).extract('//text()').getclobval(),1) output
 from source s,
-    table(dbms_workload_repository.ash_report_html(l_dbid     => s.dbid, 
-                                                   l_inst_num => s.inst_id, 
+    gv$database d,
+    table(dbms_workload_repository.ash_report_html(l_dbid     => d.dbid, 
+                                                   l_inst_num => d.inst_id, 
                                                    l_btime    => s.start_date, 
                                                    l_etime    => s.end_date,
                                                    l_sid      => s.sid,
                                                    l_sql_id   => s.sql_id)) t
-group by s.name, s.dbid, s.inst_id, s.start_date, s.end_date, s.sid, s.sql_id;
+group by d.name, d.dbid, d.inst_id, s.start_date, s.end_date, s.sid, s.sql_id;
