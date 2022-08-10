a343d8e4974933e065578282f7d655d1f857127c
Dmitry Bykov <42538722+heccrbq@users.noreply.github.com>
2022-08-10 11:37:21 +0300

    Update cpu_usage.sql

--- hcf/cpu_usage.sql
+++ hcf/cpu_usage.sql
@@ -48,14 +48,19 @@ select
     io.consistent_gets,
     io.physical_reads,
     io.block_changes,
-    io.consistent_changes
+    io.consistent_changes,
+    o.object_name,
+    sql.program_line#
+--    sql.sql_fulltext
 from v$session s
-   join v$sesstat t on t.sid = s.sid
-   join v$statname n on n.statistic# = t.statistic#
-   join v$sess_io io on io.sid = s.sid
+    join v$sesstat t on t.sid = s.sid
+    join v$statname n on n.statistic# = t.statistic#
+    join v$sess_io io on io.sid = s.sid
+    left join v$sql sql on sql.sql_id = s.sql_id and sql.child_number = s.sql_child_number
+    left join dba_objects o on o.object_id = sql.program_id
 where s.status = 'ACTIVE'
-   and n.name like '%CPU used by this session%'
-   and s.username is not null
+    and n.name like '%CPU used by this session%'
+    and s.username is not null
 group by s.type, 
     s.username,
     t.sid,
@@ -66,7 +71,10 @@ group by s.type,
     io.consistent_gets,
     io.physical_reads,
     io.block_changes,
-    io.consistent_changes
+    io.consistent_changes,
+    o.object_name,
+    sql.program_line#
+--    sql.sql_fulltext
 order by 7 desc;
 
 
