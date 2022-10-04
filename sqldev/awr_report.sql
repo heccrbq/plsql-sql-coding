7f451cae3af7fb063c7620f7a57ed3123b3b15f8
Dmitry Bykov <42538722+heccrbq@users.noreply.github.com>
2022-10-04 10:47:07 +0300

    Update awr_report.sql

--- sqldev/awr_report.sql
+++ sqldev/awr_report.sql
@@ -42,10 +42,10 @@ with source as (
 ),
 awr as (
     select 
-        d.name, d.dbid, d.inst_id, lt.start_snap_id, lt.end_snap_id
+        d.name, d.dbid, d.inst_id, lt.start_snap_id, lt.end_snap_id, lt.bit, lt.eit
     from gv$database d,
     lateral(
-        select min(snap_id) start_snap_id, max(snap_id) end_snap_id
+        select min(snap_id) start_snap_id, max(snap_id) end_snap_id, min(sn.begin_interval_time) bit, max(sn.end_interval_time) eit
         from dba_hist_snapshot sn, source s
         where sn.dbid = d.dbid and sn.instance_number = d.inst_id 
             and (s.btime between sn.begin_interval_time and sn.end_interval_time 
@@ -53,11 +53,11 @@ awr as (
 )
 --select * From awr;
 select
-    'AWR_' || lower(awr.name) || '_' || awr.inst_id || '_' || awr.start_snap_id || '_' || awr.end_snap_id || '.html' file#,
+    'AWR_' || lower(awr.name) || '_' || awr.inst_id || '_' || to_char(awr.bit, 'hh24mi') || '_' || to_char(awr.eit, 'hh24mi') || '.html' file#,
     dbms_xmlgen.convert(xmlagg(xmlelement(output, t.output || chr(10)) order by rownum).extract('//text()').getclobval(),1) output
 from awr,
     table(dbms_workload_repository.awr_report_html(l_dbid     => awr.dbid, 
                                                    l_inst_num => awr.inst_id, 
                                                    l_bid      => awr.start_snap_id, 
                                                    l_eid      => awr.end_snap_id)) t
-group by awr.name, awr.dbid, awr.inst_id, awr.start_snap_id, awr.end_snap_id;
+group by awr.name, awr.dbid, awr.inst_id, awr.start_snap_id, awr.end_snap_id, awr.bit, awr.eit;
