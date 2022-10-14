d00c1f87302fe0f6dd33e322cdda38e52bf6633a
Dmitry Bykov <42538722+heccrbq@users.noreply.github.com>
2022-10-14 10:56:35 +0300

    Update sql_monitoring_report.sql

--- sqldev/sql_monitoring_report.sql
+++ sqldev/sql_monitoring_report.sql
@@ -15,5 +15,6 @@ select
     'SQLMONITOR_' || sql_id || '.html' file#,
     dbms_sqltune.report_sql_monitor(sql_id =>       sql_id, 
                                     report_level => 'all', 
+--                                    sql_exec_id =>  100500,
                                     type =>         'html') output
 from source;
